edd4b9260a4ead51c6d79afe65ab0439589b98d9
Gotcha7770 <gbakanov.box@gmail.com>
2026-07-29 00:56:42 +0300

    add first Kata

---
+++ FS.Kata/Refactor_CSharp_Code_Kata.fs
@@ -0,0 +1,3 @@
+module FS.Kata.Kata_1
+
+// Refactor code from Refactor_CSharp_Code_Kata_Reference.cs
---
+++ FS.Kata/Refactor_CSharp_Code_Kata_Reference.cs
@@ -0,0 +1,44 @@
+﻿#!/usr/bin/env dotnet
+
+//https://www.linkedin.com/posts/milan-jovanovic_here-are-8-tips-to-make-your-code-clean-activity-7487031571078823936-At9X?utm_source=share&utm_medium=member_desktop&rcm=ACoAADEBiPYBkFsbI9rBcOSjHffys8vFu3Ghy0k
+
+void Process(Order? order)
+{
+    if (order != null)
+    {
+        if (order.IsVerified)
+        {
+            if (order.Items.Count > 0)
+            {
+                if (order.Items.Count > 15)
+                {
+                    throw new Exception(
+                        "The order " + order.Id + " has too many items");
+                }
+
+                if (order.Status != "ReadyToProcess")
+                {
+                    throw new Exception(
+                        "The order " + order.Id + " isn't ready to process");
+                }
+
+                order.IsProcessed = true;
+            }
+        }
+    }
+}
+
+record OrderItem
+{
+    public required string Id { get; init; }
+}
+
+record Order
+{
+    public required string Id { get; init; }
+    public bool IsVerified { get; init; }
+    public ICollection<OrderItem> Items { get; init; } = [];
+    
+    public string Status { get; set; }
+    public bool IsProcessed { get; set; }
+}
