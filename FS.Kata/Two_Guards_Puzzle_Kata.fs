ad03031bd67ae5f3859836160f32a3328f78a8fc
Gotcha7770 <gbakanov.box@gmail.com>
2026-08-16 19:40:27 +0300

    add 2 guards kata

---
+++ FS.Kata/Two_Guards_Puzzle_Kata.fs
@@ -0,0 +1,5 @@
+module FS.Kata.Two_Guards_Puzzle_Kata
+
+// There are two doors. One leads to paradise, the other to hell.
+// There are two guards. One always tells the truth, and the other always lies.
+// You may ask one guard one question to determine the correct door.
