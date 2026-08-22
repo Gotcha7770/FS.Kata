5150b713d5d322fe22724e95fd648277def66f88
Gotcha7770 <gbakanov.box@gmail.com>
2026-08-23 01:21:23 +0300

    Selector as monad

--- FS.Kata/Two_Guards_Puzzle_Kata.fs
+++ FS.Kata/Two_Guards_Puzzle_Kata.fs
@@ -38,10 +38,10 @@ let a6 = 2 + 2 |> (is 4 |> (truth' >> lie'))
 
 open System
 
-type Selector<'a> = private Selector of (unit -> 'a)
+type Selector<'a> = private Selector of (Entrances -> 'a)
 
-type Entrances() =
-    let run (Selector f) = f ()
+and Entrances() =
+    let run (Selector f) entrances = f entrances
 
     let shuffle (list: 'a list) =
         let array = List.toArray list
@@ -64,37 +64,47 @@ type Entrances() =
     let guards = shuffle [ Honest; Liar ]
 
     member private _.LeftDoorValue = doors[0]
-    member this.LeftDoor = Selector(fun () -> this.LeftDoorValue)
+    member _.LeftDoor = Selector _.LeftDoorValue
     member private _.RightDoorValue = doors[1]
-    member this.RightDoor = Selector(fun () -> this.RightDoorValue)
+    member _.RightDoor = Selector _.RightDoorValue
 
     member private _.LeftGuard = guards[0]
     member private _.RightGuard = guards[1]
 
-    member this.AskLeftGuard predicate selector =
-        let value = run selector
+    member _.AskLeftGuard predicate selector =
+        Selector(fun env -> answer env.LeftGuard predicate (run selector env))
 
-        answer this.LeftGuard predicate value
+    member _.AskRightGuard predicate selector =
+        Selector(fun env -> answer env.RightGuard predicate (run selector env))
 
-    member this.AskRightGuard predicate selector =
-        let value = run selector
-
-        answer this.RightGuard predicate value
+    member _.Reveal selector = run selector
 
 let state = Entrances()
 
 module Selector =
     let from value = Selector(fun _ -> value)
+    let map f (Selector g) = Selector(g >> f)
+
+    let bind (f: 'a -> Selector<'b>) (Selector g) =
+        Selector(fun env ->
+            let (Selector h) = f (g env)
+            h env)
 
 // Questions to private Guard
-let a7 = Selector.from (2 + 2) |> (is 4 |> state.AskLeftGuard)
+let a7 = (Selector.from (2 + 2) |> (is 4 |> state.AskLeftGuard) |> state.Reveal) state
 
 // Questions about private state
-let a8 = state.LeftDoor |> (is Hell |> state.AskLeftGuard)
+let a8 = (state.LeftDoor |> (is Hell |> state.AskLeftGuard) |> state.Reveal) state
+//let a8 = state.AskLeftGuard <| is Hell <| state.LeftDoor |> state.Reveal <| state
 
-let a9 =
-    state.LeftDoor
-    |> (is Hell |> state.AskLeftGuard)
-    |> Selector.from
-    |> (is true |> state.AskRightGuard)
-//let a9 = state.LeftDoor |> (is Hell |> (state.AskLeftGuard >> state.AskRightGuard))
+// let a9 =
+//     state.LeftDoor
+//     |> (is Hell |> state.AskLeftGuard)
+//     |> (is true |> state.AskRightGuard)
+//     |> state.Reveal <| state
+
+//let a9 = (state.LeftDoor |> ((is Hell |> state.AskLeftGuard) >> (is true |> state.AskRightGuard)) |> state.Reveal) state
+let a9 = state.AskRightGuard <| is true << (state.AskLeftGuard <| is Hell) <| state.LeftDoor |> state.Reveal <| state
+
+// Quantum state
+let a10 = state.Reveal state.LeftDoor <| state
